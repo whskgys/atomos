247960469695f7cb9efd2497344af0eae1edc6b8
jonahyun <whskgys@naver.com>
2023-06-27 16:43:03 +0900

    commit 2

---
+++ turorial_0627_2/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-6 mt-5 mx-auto">
+        <form v-on:submit.prevent="login" action="">
+          <h2 class="h3 mb-3 font-weight-normal">로그인하기</h2>
+          <div class="form-group">
+            <label for="email">이메일주소</label>
+            <input type="email" v-model="email" class="form-control" placeholder="이메일주소" />
+          </div>
+          <div class="form-group">
+            <label for="password">암호</label>
+            <input type="password" v-model="password" class="form-control" placeholder="암호" />
+          </div>
+          <button class="btn btn-lg btn-primary btn-block" type="submit">로그인하기</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data:() => ({
+    email: '',
+    password: '',
+  })
+  
+}
+</script>
---
+++ turorial_0627_2/pages/introduction.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>자기소개 페이지 입니다.
+    <nuxt-link to="/">홈으로 돌아가기</nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'introduction'
+}
+</script>
